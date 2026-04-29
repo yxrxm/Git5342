9fed183fdd604d72af86059ebd3577fb838f9213
shinyr1128 <shinyr1204@gmail.com>
2026-04-29 10:34:13 +0900

    Modify mynumber to print odd and even numbers

--- mynumber.c
+++ mynumber.c
@@ -2,9 +2,17 @@
 
 int main(void)
 {
+    printf("Odd numbers between 1 and 100:\n");
     for (int i = 1; i <= 100; i += 2) {
-        printf("%d\n", i);
+        printf("%d ", i);
     }
+
+    printf("\n\nEven numbers between 1 and 100:\n");
+    for (int i = 2; i <= 100; i += 2) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
 
     return 0;
 }
