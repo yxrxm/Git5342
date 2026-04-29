cf10aeed7d6fd42c11a6a7189e2f435ff2c1e57b
shinyr1128 <shinyr1204@gmail.com>
2026-04-29 10:36:45 +0900

    Modify mynumber to print odd even and prime numbers

--- mynumber.c
+++ mynumber.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int isPrime(int n)
+{
+    if (n < 2) {
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 int main(void)
 {
     printf("Odd numbers between 1 and 100:\n");
@@ -10,6 +25,13 @@ int main(void)
     printf("\n\nEven numbers between 1 and 100:\n");
     for (int i = 2; i <= 100; i += 2) {
         printf("%d ", i);
+    }
+
+    printf("\n\nPrime numbers between 1 and 100:\n");
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
     }
 
     printf("\n");
